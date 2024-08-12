8f4c99c1c5f7a6b9553ad9c2a07a64bcec424265
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-08-12 12:52:22 +0530

    what we offer completed

--- src/components/home/_what-we-offer/page.js
+++ src/components/home/_what-we-offer/page.js
@@ -10,10 +10,43 @@ export default function WhatWeOffer(){
                 <div className='category-cards-container'>
                     <div className='catergory-card'>
                         <img src="/assets/vegetable-skel.png" alt="what we offer category card image" />
-                        {/* <div className='card-content'>
-                            <h4>Vegetables</h4>
+                        <div className='header-img-1'>
+                            <img src="/assets/vegetable.png"  alt="Vegetable image" />
+                        </div>
+                        <div className='card-content'>
+                            <h5>Vegetables</h5>
                             <p>When nothing prevents our to we like best be</p>
-                        </div> */}
+                        </div>
+                    </div>
+                    <div className='catergory-card'>
+                        <img src="/assets/fruits-skel.png" alt="what we offer category card image" />
+                        <div className='header-img-2'>
+                            <img src="/assets/fruits.png"  alt="Vegetable image" />
+                        </div>
+                        <div className='card-content'>
+                            <h5>Fruits</h5>
+                            <p>When nothing prevents our to we like best be</p>
+                        </div>
+                    </div>
+                    <div className='catergory-card'>
+                        <img src="/assets/corn-skel.png" alt="what we offer category card image" />
+                        <div className='header-img-3'>
+                            <img src="/assets/corn.png"  alt="Vegetable image" />
+                        </div>
+                        <div className='card-content'>
+                            <h5>Corn</h5>
+                            <p>When nothing prevents our to we like best be</p>
+                        </div>
+                    </div>
+                    <div className='catergory-card'>
+                        <img src="/assets/rice-skel.png" alt="what we offer category card image" />
+                        <div className='header-img-4'>
+                            <img src="/assets/rice.png"  alt="Vegetable image" />
+                        </div>
+                        <div className='card-content'>
+                            <h5>Rice</h5>
+                            <p>When nothing prevents our to we like best be</p>
+                        </div>
                     </div>
                 </div>
             </div>
